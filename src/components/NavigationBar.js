bdae49e369ccc08a075ab73918bd03a8c7aeaa3a
josua-yoprisyanto <josua.y8@gmail.com>
2023-01-30 21:40:36 +0700

    fix scroll

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import { Navbar, Container } from 'react-bootstrap'
-import '../assets/css/navbar.css'
-import { Link } from 'react-scroll'
+import React from "react";
+import { Navbar, Container } from "react-bootstrap";
+import "../assets/css/navbar.css";
+import { Link } from "react-scroll";
 const NavigationBar = () => {
-    return (
-        <>
-            <Navbar variant="dark" expand="lg" className="mt-2">
-                <Container fluid>
-                    <Navbar.Brand href="#">COVID-19 | INDONESIA</Navbar.Brand>
-                    <Navbar.Toggle aria-controls="navbarScroll" />
-                    <Navbar.Collapse className="justify-content-end">
-                        <Navbar.Text>
-                            <li>
-                                <Link to="definition" spy={true} smooth={true} offset={50} duration={500}>
-                                    Pengertian
-                                </Link>
-                            </li>
-                        </Navbar.Text>
-                        <Navbar.Text className="symptoms-pc">
-                            <li>
-                                <Link to="symptoms-pc" spy={true} smooth={true} offset={50} duration={500}>
-                                    Gejala
-                                </Link>
-                            </li>
-                        </Navbar.Text>
-                        <Navbar.Text className="symptoms-mobile">
-                            <li>
-                                <Link to="symptoms-mobile" spy={true} smooth={true} offset={50} duration={500}>
-                                    Gejala
-                                </Link>
-                            </li>
-                        </Navbar.Text>
-                        <Navbar.Text>
-                            <li>
-                                <Link to="solution" spy={true} smooth={true} offset={50} duration={500}>
-                                    Solusi
-                                </Link>
-                            </li>
-                        </Navbar.Text>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar variant="dark" expand="lg" className="mt-2">
+        <Container fluid>
+          <Navbar.Brand href="#">COVID-19 | INDONESIA</Navbar.Brand>
+          <Navbar.Toggle aria-controls="navbarScroll" />
+          <Navbar.Collapse className="justify-content-end">
+            <Navbar.Text>
+              <li>
+                <Link to="definition" offset={-50} duration={500}>
+                  Pengertian
+                </Link>
+              </li>
+            </Navbar.Text>
+            <Navbar.Text className="symptoms-pc">
+              <li>
+                <Link to="symptoms-pc" offset={-50} duration={500}>
+                  Gejala
+                </Link>
+              </li>
+            </Navbar.Text>
+            <Navbar.Text className="symptoms-mobile">
+              <li>
+                <Link to="symptoms-mobile" offset={-50} duration={500}>
+                  Gejala
+                </Link>
+              </li>
+            </Navbar.Text>
+            <Navbar.Text>
+              <li>
+                <Link to="solution" offset={-50} duration={500}>
+                  Solusi
+                </Link>
+              </li>
+            </Navbar.Text>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </>
+  );
+};
 
-export default NavigationBar
+export default NavigationBar;
